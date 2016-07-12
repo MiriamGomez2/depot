ade7102d6feef8f76179e2134d9f903e696aa57e
fcruzPS <fcruz0126@gmail.com>
2016-07-12 13:29:59 -0500

    Prettier listings

--- app/models/product.rb
+++ app/models/product.rb
@@ -5,5 +5,11 @@ class Product < ApplicationRecord
   validates :image_url, allow_blank: true, format: {
       with:    %r{\.(gif|jpg|png)\Z}i,
       message: 'must be a URL for GIF, JPG or PNG image.'
-  }
+    }
+  validates :title, length: {minimum: 10}
+
+end
+
+def self.latest
+  order(:updated_at).last
 end
