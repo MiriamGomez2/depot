3d0515c1f40b09081c885746dd8ed258cff4bc4a
fcruzPS <fcruz0126@gmail.com>
2016-07-14 02:34:25 -0500

    carro

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,7 +9,7 @@ class ApplicationController < ActionController::Base
   def authorize
     if request.format == Mime[:html]
       unless User.find_by(id: session[:user_id])
-       redirect_to login_url, notice: "Please log in"
+       # redirect_to login_url, notice: "Please log in"
       end
     else
       authenticate_or_request_with_http_basic do |username, password|
